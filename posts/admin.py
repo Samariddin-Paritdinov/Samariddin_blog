c6c760b049edd7392f02e38847d7f7c459bc2707
Samariddin-Paritdinov <muzaffarovichsamarr@gmail.com>
2025-04-28 11:17:13 +0500

    1st lesson done

---
+++ posts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import  Post
+# Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'owner__username', 'created_at', 'updated_at',)
+    list_display_links = ('id', 'title', 'owner__username',)
+    list_filter = ('created_at', 'updated_at',)
+    search_fields = ('title', 'description',)
+    readonly_fields = ('view_count',)
